557e184e0fe8702fabdf651eadae6fe02d3181bc
Michael Pozhidaev <msp@luwrain.org>
2020-08-24 12:53:01 +0700

    Adding UsersTest.confirmLimitExceeding()

--- src/test/java/org/luwrain/io/api/books/v1/UsersTest.java
+++ src/test/java/org/luwrain/io/api/books/v1/UsersTest.java
@@ -16,21 +16,53 @@ package org.luwrain.io.api.books.v1;
 
 import java.io.*;
 import org.junit.*;
+import org.luwrain.io.api.books.v1.users.*;
 
 public final class UsersTest extends Base
 {
-    @Test public void collection() throws IOException
-    {
-	if (!isReady())
-	    return;
-	getAccessToken();
-	//	final Book[] books = newBooks().users().collection().exec();
-    }
-
         @Test public void register() throws IOException
     {
 	if (!isReady())
 	    return;
 	assertTrue(newBooks().users().register().mail("test@test.org").passwd("0123456789").exec().isOk());
     }
+
+        @Test public void confirmLimitExceeding() throws IOException
+    {
+	final String mail = "test-confirm-limit-exceeding@luwrain.org";
+	if (!isReady())
+	    return;
+	final RegisterQuery.Response r1 = newBooks().users().register().mail(mail).passwd("123123123").exec();
+	assertNotNull(r1);
+	assertTrue(r1.isOk());
+	for(int i = 0;i < 5;i++)
+	{
+	    try {
+		final ConfirmQuery.Response r = newBooks().users().confirm().mail(mail).confirmationCode("10000").exec();
+		assertTrue(false);
+	    }
+	    catch(BooksException e)
+	    {
+		final ErrorResponse r = e.getErrorResponse();
+		assertNotNull(r);
+		assertNotNull(r.getType());
+		assertEquals(r.getType(), "INVALID_CONFIRMATION_CODE");
+	    }
+	}
+
+		    try {
+			final ConfirmQuery.Response r = newBooks().users().confirm().mail(mail).confirmationCode("10000").exec();
+		assertTrue(false);
+	    }
+	    catch(BooksException e)
+	    {
+		final ErrorResponse r = e.getErrorResponse();
+		assertNotNull(r);
+		assertNotNull(r.getType());
+		assertEquals(r.getType(), "TOO_MANY_ATTEMPTS");
+	    }
+
+		    
+    }
+
 }
